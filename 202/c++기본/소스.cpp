ca72d1ef7fdebebdf8bb628d8e95a6ec58b84b94
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-03-17 12:12:18 +0900

    using namespace std 를 통해 출력과 개행에 std :: 를 사용할 필요 x

--- 202/c++기본/소스.cpp
+++ 202/c++기본/소스.cpp
@@ -1,10 +1,13 @@
 //cout, endl 객체를 가지고 있음.
 #include<iostream>
 
+//std 안 써도 된다.
+using namespace std;
+
 int main(void)
 {
 	//cout : printf, endl(endline) : \n
-	std :: cout << ("안녕 세상아") << std::endl;
+	cout << ("안녕 세상아") << endl;
 
 	return 0;
 }
