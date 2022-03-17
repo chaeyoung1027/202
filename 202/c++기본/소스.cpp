4d1f9e8cdf61e0a1085e2723647c766bac6bdd72
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-03-17 12:10:16 +0900

    c++ iostream을 통해 cout, endl 사용

--- 202/c++기본/소스.cpp
+++ 202/c++기본/소스.cpp
@@ -1,8 +1,10 @@
-#include<stdio.h>
+//cout, endl 객체를 가지고 있음.
+#include<iostream>
 
 int main(void)
 {
-	printf("안녕 세상아\n");
+	//cout : printf, endl(endline) : \n
+	std :: cout << ("안녕 세상아") << std::endl;
 
 	return 0;
 }
