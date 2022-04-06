019c2e4a97728604a1cde0bdf1a7683980441732
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-04-06 10:06:37 +0900

    call by 레퍼런스

--- 202/c++기본/02-04 call by 레퍼런스.cpp
+++ 202/c++기본/02-04 call by 레퍼런스.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-void plus2(int* num) {
-	*num2+=2
+void plus2(int& num) {
+	num += 2;
 }
 
 int main(void) {
 	int a = 3;
-	plus2(&a);
+	plus2(a);
 	cout << a << endl;
 
 	return 0;
