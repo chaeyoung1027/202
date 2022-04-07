db8fb65c870d62a8c9108b2a08d6b59df4740a0f
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-04-07 10:59:19 +0900

    디폴트 매개변수(함수 오버로딩과는 별개)

--- 202/c++기본/02-05 함수 오버로딩.cpp
+++ 202/c++기본/02-05 함수 오버로딩.cpp
@@ -11,28 +11,18 @@ using namespace std;
 //함수 이름은 같고 매개변수는 다르다.(함수 오버로딩)
 
 //default값을 채울 때에는 뒤에서부터 채워야한다.
-int jegop(int A, int B) {
+int jegop(int A, int B = 1) {
 	return A * A + B;
 }
 
-double jegop(double A, double B) {
+double jegop(double A, double B = 1.1) {
 	return A * A + B;
-}
-
-int jegop(int A) {
-	return A * A;
-}
-
-double jegop(double A) {
-	return A * A;
 }
 
 int main(void)
 {
-	cout << (4, 1) << endl;//17
-	cout << (3.3, 1.1) << endl;//11.99
-	cout << jegop(3 + 1) << endl;	//16
-	cout << jegop(3.3) << endl;		//10.89
+	cout << jegop(4) << endl;	//jegop(4, 1) : 17
+	cout << jegop(3.3) << endl;	//jegop(3.3, 1.1) : 11.99
 
 	return 0;
 }
