ca311226d63eb825b6c64c94ad66b0cc62790d89
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-04-07 10:51:49 +0900

    함수 오버로딩

--- 202/c++기본/02-05 함수 오버로딩.cpp
+++ 202/c++기본/02-05 함수 오버로딩.cpp
@@ -8,7 +8,17 @@ using namespace std;
 //#define jegop(A) ((A)*(A))
 
 
-//함수 이름은 같고 매개변수는 다르다.
+//함수 이름은 같고 매개변수는 다르다.(함수 오버로딩)
+
+//default값을 채울 때에는 뒤에서부터 채워야한다.
+int jegop(int A, int B) {
+	return A * A + B;
+}
+
+double jegop(double A, double B) {
+	return A * A + B;
+}
+
 int jegop(int A) {
 	return A * A;
 }
@@ -19,8 +29,10 @@ double jegop(double A) {
 
 int main(void)
 {
+	cout << (4, 1) << endl;//17
+	cout << (3.3, 1.1) << endl;//11.99
 	cout << jegop(3 + 1) << endl;	//16
-	cout << jegop(3.3) << endl;		//16
+	cout << jegop(3.3) << endl;		//10.89
 
 	return 0;
 }
