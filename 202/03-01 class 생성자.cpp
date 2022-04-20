9d38ed0d8b7cc8612359ae11c7d2deb94a67b3ed
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-04-20 09:17:34 +0900

    생성자호출

---
+++ 202/03-01 class 생성자.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Student
+{
+private:
+	int nHakbun;
+	string sName;
+
+public:
+
+	//생성자 : 객체가 생성될 때, 호출되는 함수
+	Student(void) {
+		nHakbun = 1234;
+		sName = "이사랑";
+		cout << "학번이 등록되었습니다." << endl;
+	}
+
+};
+
+int main(void)
+{
+	Student stu1;
+	Student stu2 = Student();
+
+	return 0;
+}
