3d5d5f3ce56493f1a8ec28cbbbfe9bb83ac330c0
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-05-11 10:09:02 +0900

    멤버변수 생성과 동시에 초기화

--- 202/03-01 class 생성자.cpp
+++ 202/03-01 class 생성자.cpp
@@ -29,14 +29,11 @@ int main(void)
 }
 
 
-Student::Student() {
-	this->nHakbun = 1234;
-	this->sName = "이사랑";
-	cout << "학번이 등록되었습니다." << endl;
-}
-Student::Student(int Hakbun, string Name) {
-	this->nHakbun = Hakbun;
-	this->sName = Name;
+// 멤버변수를 초기화 할 수 있으며 따라서,
+// const형 변수와 참조형 변수를 멤버변수로 할 수 있다.
+Student::Student(int Hakbun, string Name)
+	: nHakbun(Hakbun), sName(Name)	// 멤버변수(매개변수)
+{
 	cout << "학번이 등록되었습니다." << endl;
 }
 
