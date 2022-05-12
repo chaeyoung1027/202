7fc26edb694dd4e0f6141f5b89a6b4545591b33f
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-05-12 11:07:44 +0900

    멤버변수 생성과 동시에 초기화

--- 202/c++기본/03-01 class 생성자.cpp
+++ 202/c++기본/03-01 class 생성자.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<string>
+#include <iostream>	// cout, endl 사용
+#include <string>
 
 using namespace std;
 
@@ -9,15 +9,45 @@ private:
 	int nHakbun;
 	string sName;
 
-	//생성자 : 객체가 생성될 때, 호출되는 함수
-	//디폴트 생성자를 컴파일러가 만들어준다.
-	//Student(){}
+public:
+	// 생성자 : 객체가 생성되면 자동으로 호출되는 함수
+	// 반환형을 갖고있지 않는다
+	Student();
+	Student(int Hakbun, string Name);
+
+	void show();
+
 };
+
+Student::Student()
+{
+	this->nHakbun = 1234;
+	this->sName = "이사랑";
+	cout << "학번이 등록되었습니다." << endl;
+}
+
+// 멤버변수를 초기화 할 수 있으며 따라서,
+// const형 변수와 참조형 변수를 멤버변수로 할 수 있다.
+Student::Student(int Hakbun, string Name)
+	: nHakbun(Hakbun), sName(Name)	// 멤버변수(매개변수)
+{
+	cout << "학번이 등록되었습니다." << endl;
+}
+
+void Student::show()
+{
+	cout << "학번은 " << nHakbun << "입니다" << endl;
+	cout << "이름은 " << sName << "입니다" << endl << endl;
+}
 
 int main(void)
 {
-	Student stu1;
-	Student stu2 = Student();
+	Student stu1 = Student();
+	stu1.show();
 
-	return 0;
+	Student stu2 = Student(1111, "JWP");
+	stu2.show();
+
+
+
 }
