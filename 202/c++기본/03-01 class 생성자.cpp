2dc323ebc63c94605bb411b1470030f82013d46e
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-05-18 09:47:10 +0900

    복사생성자(얕은복사)

--- 202/c++기본/03-01 class 생성자.cpp
+++ 202/c++기본/03-01 class 생성자.cpp
@@ -1,51 +1,64 @@
-//cout, endl 객체를 가지고 있음
-#include <iostream>
-#include <string.h>
+#include <iostream>   //cout, endl 객체를 가지고 있음
+#include <string.h>   
 
 using namespace std;
 
-class Student {
-	//private:
-public:
+class Student
+{
+private:
 	int nHakbun;
-	//string sName;
 	char* sName;
-	Student(int Hakbun, const char* name);
 
-	//소멸자 : 객체가 메모리에서 해제될 때 호출되는 함수
+public:
+
+	//생성자: 객체가 생성될 때 호출되는 함수
+	Student(void);
+	Student(int Hakbun, const char* Name);
+	Student(const Student& rhs);
+
+	//소멸자: 객체가 메모리에서 해제될 떄, 호출되는 함수
 	~Student();
 	void show(void);
 };
 
-int main(void) {
+int main(void)
+{
+	//일반 생성자 호출
 	Student stu1 = Student(1111, "JWP");
+	//복사 생성자 호출
+	Student stu2 = stu1;
+
 	stu1.show();
+	stu2.show();
 
 	return 0;
 }
-//Student::Student(int Hakbun, string name) {
-//   this->nHakbun = Hakbun;
-//   this->sName = name;
-//   cout << "학번이 등록 되었습니다." << endl;
-//}
 
-Student::Student(int Hakbun, const char* name)
-//객체 생성과 동시에 맴버변수 초기화   //멤버 변수(매개변수)
-//const/참조형 멤버변수를 사용할 수 있다.
+Student::Student(int Hakbun, const char* Name)
+// 객체 생성과 동시에 멤버변수 초기화   //형식 - 멤버변수(매개변수)
+// const/참조형 멤버변수를 사용할 수 있다.
 	:nHakbun(Hakbun)
 {
-	cout << "일반 생성자 호출" << endl;
-	int len = strlen(name) + 1;      //공간개수 측정
-	sName = new char[len];         //공간생성
-	strcpy(sName, name);            //매개변수에 있는 문자열 복사
+	cout << "일반생성자 호출" << endl;
+	int len = strlen(Name) + 1;   //동적할당만큼 공간의 크기를 구하고
+	sName = new char[len];   //그 크기만큼 배열로 동적할당
+	strcpy(sName, Name);   //매개변수에 있는 문자열 복사
 }
 
-Student::~Student() {
-	delete[]sName;
-	cout << "소멸자 호출" << endl;
-}
-
-void Student::show(void) {
+void Student::show(void)
+{
 	cout << "학번은 " << nHakbun << "입니다." << endl;
 	cout << "이름은 " << sName << "입니다." << endl << endl;
+}
+
+Student::Student(const Student& rhs)
+	:nHakbun(rhs.nHakbun), sName(rhs.sName)
+{
+	cout << "복사생성자" << endl;
+}
+
+Student::~Student()
+{
+	delete[]sName;
+	cout << "소멸자 호출" << endl;
 }
