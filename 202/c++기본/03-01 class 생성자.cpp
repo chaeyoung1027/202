8cad28808551d4b78d48b7322c4c7e93fd2acfcb
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-05-18 09:01:22 +0900

    string 을 char* 형식으로 전환

--- 202/c++기본/03-01 class 생성자.cpp
+++ 202/c++기본/03-01 class 생성자.cpp
@@ -1,53 +1,51 @@
-#include <iostream>	// cout, endl 사용
-#include <string>
+//cout, endl 객체를 가지고 있음
+#include <iostream>
+#include <string.h>
 
 using namespace std;
 
-class Student
-{
-private:
-	int nHakbun;
-	string sName;
-
+class Student {
+	//private:
 public:
-	// 생성자 : 객체가 생성되면 자동으로 호출되는 함수
-	// 반환형을 갖고있지 않는다
-	Student();
-	Student(int Hakbun, string Name);
-
-	void show();
-
+	int nHakbun;
+	//string sName;
+	char* sName;
+	Student(int Hakbun, const char* name);
+	~Student();
+	void show(void);
 };
 
-Student::Student()
-{
-	this->nHakbun = 1234;
-	this->sName = "이사랑";
-	cout << "학번이 등록되었습니다." << endl;
-}
-
-// 멤버변수를 초기화 할 수 있으며 따라서,
-// const형 변수와 참조형 변수를 멤버변수로 할 수 있다.
-Student::Student(int Hakbun, string Name)
-	: nHakbun(Hakbun), sName(Name)	// 멤버변수(매개변수)
-{
-	cout << "학번이 등록되었습니다." << endl;
-}
-
-void Student::show()
-{
-	cout << "학번은 " << nHakbun << "입니다" << endl;
-	cout << "이름은 " << sName << "입니다" << endl << endl;
-}
-
-int main(void)
-{
-	Student* stu = new Student[6];
-	for (int i = 0; i < 6; i++)
-		//배열이므로 ->를 쓰지 않고, .으로 멤버접근
-		stu[i].show();
-	
-	delete[]stu;
+int main(void) {
+	Student stu1 = Student(1111, "JWP");
+	stu1.show();
 
 	return 0;
+}
+//Student::Student(int Hakbun, string name) {
+//   this->nHakbun = Hakbun;
+//   this->sName = name;
+//   cout << "학번이 등록 되었습니다." << endl;
+//}
+
+
+
+Student::Student(int Hakbun, const char* name)
+//객체 생성과 동시에 맴버변수 초기화   //멤버 변수(매개변수)
+//const/참조형 멤버변수를 사용할 수 있다.
+	:nHakbun(Hakbun)
+{
+	cout << "일반 생성자 호출" << endl;
+	int len = strlen(name) + 1;      //공간개수 측정
+	sName = new char[len];         //공간생성
+	strcpy(sName, name);            //매개변수에 있는 문자열 복사
+}
+
+Student::~Student() {
+	delete[]sName;
+	cout << "소멸자 호출" << endl;
+}
+
+void Student::show(void) {
+	cout << "학번은 " << nHakbun << "입니다." << endl;
+	cout << "이름은 " << sName << "입니다." << endl << endl;
 }
