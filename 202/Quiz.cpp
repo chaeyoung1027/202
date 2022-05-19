4a067db71d4b699a5b1fe82eb9bc8bf485ef8268
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-05-19 12:09:15 +0900

    정답!

--- 202/Quiz.cpp
+++ 202/Quiz.cpp
@@ -1,5 +1,5 @@
 class CBox {
-private:
+public:	//또는 protected 만약 private로 하고싶다면 저 값을 바꾸게 해주는 method를 public 또는 Protected로 CBox안에 만들어야한다.
 	double width;
 };
 
@@ -9,3 +9,7 @@ public:
 		width = w;
 	}
 };
+
+int main(void) {
+
+}
