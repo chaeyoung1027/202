81155ed4c40b484fb3864a1270f6299b9fbb4f96
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-09-15 11:12:12 +0900

    생성자 수정하기...

---
+++ 202/c++기본/문제풀기.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+class PB
+{
+public:
+	PB() {}
+	PB(int name, int heart, int attack, int defense):Cname(name),Cheart(heart),Cattack(attack),Cdefense(defense){}
+	
+private:
+	string Cname;
+	int Cheart;
+	int Cattack;
+	int Cdefense;
+};
+
+int power(int pheart, int pattack, int pdefense) {
+	cout << pattack << "만큼 공격받았습니다." << endl;
+	cout << pdefense << "만큼 방어했습니다." << endl;
+	pheart = pheart - pattack+pdefense;
+	
+	return pheart;
+}
+
+int main(void) {
+	PB pb1 = { "채영", 100, 2, 1 };
+
+}
