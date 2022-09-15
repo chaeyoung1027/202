003f97a48051627570c8333a6c295feb82205abb
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-09-15 11:49:18 +0900

    기본 코드 완성

--- 202/c++기본/문제풀기.cpp
+++ 202/c++기본/문제풀기.cpp
@@ -7,7 +7,15 @@ class PB
 {
 public:
 	PB() {}
-	PB(string name, int heart, int attack, int defense):Cname(name),Cheart(heart),Cattack(attack),Cdefense(defense){}
+	PB(string name, int heart, int attack, int defense)
+		:Cname(name),Cheart(heart),Cattack(attack),Cdefense(defense){
+		cout << Cattack << "만큼 공격받았습니다." << endl;
+		cout << Cdefense << "만큼 방어했습니다." << endl;
+		Cheart = Cheart - Cattack + Cdefense;
+	}
+	void print() {
+		cout << "현재 "<<Cname<<"의 체력은 "<<Cheart<<"남았습니다." << endl;
+	}
 	
 private:
 	string Cname;
@@ -16,15 +24,7 @@ private:
 	int Cdefense;
 };
 
-int power(int pheart, int pattack, int pdefense) {
-	cout << pattack << "만큼 공격받았습니다." << endl;
-	cout << pdefense << "만큼 방어했습니다." << endl;
-	pheart = pheart - pattack+pdefense;
-	
-	return pheart;
-}
-
 int main(void) {
 	PB pb1 = { "채영", 100, 2, 1 };
-
+	pb1.print();
 }
